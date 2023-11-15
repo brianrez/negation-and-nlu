0afb4f3ba1f4937280a9419892bf2772430d9e5f
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2023-11-15 10:41:50 -0700

    download model

--- annotations-and-experiments/download_tasks.py
+++ annotations-and-experiments/download_tasks.py
@@ -14,17 +14,16 @@ DATA_DIR = "./content/exp/tasks"
 
 # Data -------------------------------------------
 os.makedirs(DATA_DIR, exist_ok=True)
-'''
+
 task_list = ["sst", "qqp", "stsb", "qnli", "copa", "wsc", "wic", "commonsenseqa"]
 # task_list = []
 
 for task in task_list:
     print("task: {}\n".format(task))
     downloader.download_data([task], DATA_DIR)
-'''
 
-# Download the roberta-base model
-# export_model.lookup_and_export_model(
+
+
 export_model.export_model(
         hf_pretrained_model_name_or_path="roberta-large",
         output_base_path="./models/roberta-large",
