031c71ac3a74d2c16db8e722e95d5586f3ee02dc
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-10-05 16:24:10 -0700

    try

--- annotations-and-experiments/call_tokenizer.sh
+++ annotations-and-experiments/call_tokenizer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 input=(
-  "--model_name bert-large-uncased --model_path bert-large-uncased-nsp-500000-1e-06-128"
-  "--model_name bert-large-uncased --model_path bert-large-uncased-pp-500000-1e-06-128"
-  "--model_name bert-large-uncased --model_path bert-large-uncased-dual-500000-1e-06-32"
-  "--model_name bert-base-uncased --model_path bert-base-uncased-nsp-500000-1e-06-64"
-  "--model_name bert-base-uncased --model_path bert-base-uncased-pp-500000-1e-06-64"
-  "--model_name bert-base-uncased --model_path bert-base-uncased-dual-500000-1e-06-32"
+  "--model_name bert-large-uncased --model_path mhr2004/bert-large-uncased-nsp-500000-1e-06-128"
+  "--model_name bert-large-uncased --model_path mhr2004/bert-large-uncased-pp-500000-1e-06-128"
+  "--model_name bert-large-uncased --model_path mhr2004/bert-large-uncased-dual-500000-1e-06-32"
+  "--model_name bert-base-uncased --model_path  mhr2004/bert-base-uncased-nsp-500000-1e-06-64"
+  "--model_name bert-base-uncased --model_path  mhr2004/bert-base-uncased-pp-500000-1e-06-64"
+  "--model_name bert-base-uncased --model_path  mhr2004/bert-base-uncased-dual-500000-1e-06-32"
 )
 
 for i in "${!input[@]}"; do
